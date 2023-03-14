e8692f766a73381729b163ecc0f7fbbd53dc2ea6
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-14 22:03:52 +0200

    Get data on form submit.

--- app.py
+++ app.py
@@ -1,32 +1,59 @@
 import os
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, request, url_for, redirect
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField
+from wtforms import StringField, DateField, SubmitField
 from wtforms.validators import DataRequired
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
 SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = SECRET_KEY
+app.config["SECRET_KEY"] = SECRET_KEY
 
 bootstrap = Bootstrap5(app)
 
+API_ACCESS_KEY = os.getenv("API_ACCESS_KEY")
 
-class Form(FlaskForm):
+
+class MyForm(FlaskForm):
     """
     Form to get data for requesting stock data.
     """
-    symbol = StringField('symbol', validators=[DataRequired()])
-    date_from = DateField('date_from', validators=[DataRequired()])
-    date_to = DateField('date_to', validators=[DataRequired()])
+
+    symbol = StringField("symbol", validators=[DataRequired()])
+    date_from = DateField("date_from", validators=[DataRequired()])
+    date_to = DateField("date_to", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    form = Form()
-    return render_template('index.html', form=form)
+    form = MyForm()
+    if request.method == "POST":
+        # if form.validate_on_submit():
+        symbol = form.symbol.data
+        date_from = form.date_from.data
+        date_to = form.date_to.data
+        return redirect(f'/stock_data/{symbol}/{date_from}/{date_to}')
+
+    return render_template("index.html", form=form)
 
 
-if __name__ == '__main__':
+@app.route("/stock_data/<symbol>/<date_from>/<date_to>")
+def stock_data(symbol, date_from, date_to):
+    response = requests.get(f"http://api.marketstack.com/v1/eod?access_key={API_ACCESS_KEY}&symbols={symbol}")
+    data = response.json()["data"]
+    return render_template("data.html", data=data)
+
+
+@app.route('/test')
+def test():
+    return "Done Testing!"
+
+
+if __name__ == "__main__":
     app.run(debug=True)
